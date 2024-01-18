2a36bc04c8dba9507488d99bc2b1113c5b14e8e4
YeJingxin515 <1771357098@qq.com>
2024-01-19 00:23:22 +0800

    1.18开会讨论界面修改

--- src/js/explore.js
+++ src/js/explore.js
@@ -15,23 +15,24 @@ var filteredPoetryList = []
 
 
 //筛选诗词
-function filterPoetry() {
+async function filterPoetry() {
     searchInfo.originName = document.getElementById('select-originName').value;
+    searchInfo.author = document.getElementById('input-author').value;
     searchInfo.theme = document.getElementById('select-theme').value;
     searchInfo.method = document.getElementById('select-method').value;
     searchInfo.emotion = document.getElementById('select-emotion').value;
+
     filteredPoetryList = poetryArray.filter(poetry => {
         return (
             (searchInfo.originName === '' || searchInfo.originName === '全部' || poetry.originName.includes(searchInfo.originName)) &&
+            (searchInfo.author === '' || poetry.author.includes(searchInfo.author)) &&
             (searchInfo.theme === '' || searchInfo.theme === '全部' || poetry.theme.includes(searchInfo.theme)) &&
             (searchInfo.method === '' || searchInfo.method === '全部' || poetry.method.includes(searchInfo.method)) &&
             (searchInfo.emotion === '' || searchInfo.emotion === '全部' || poetry.emotion.includes(searchInfo.emotion))
             // Add more conditions for additional filters
         );
     });
-
-    updateSkyCanvas(filteredPoetryList);
-    // createStars(filteredPoetryList)
+    await createStars(filteredPoetryList)
 }
 
 //重置按钮
@@ -45,25 +46,7 @@ function resetFilters() {
 }
 
 // Attach click event listener to the reset button
- document.getElementById('reset-btn').addEventListener('click', resetFilters);
-
-
-function updateSkyCanvas(poetryData) {
-    const skyCanvas = document.getElementById('sky-canvas');
-    skyCanvas.innerHTML = '';
-
-    poetryData.forEach(poetry => {
-        const poetryDiv = document.createElement('div');
-        poetryDiv.innerHTML = `
-            <p><strong>${poetry.title}</strong></p>
-        `;
-        poetryDiv.addEventListener('click', function () {
-            updatePanelInfo(poetry);
-            showPoetryPanel()
-        });
-        skyCanvas.appendChild(poetryDiv);
-    });
-}
+document.getElementById('reset-btn').addEventListener('click', resetFilters);
 
 
 //显示详细诗词的右侧panel
@@ -78,9 +61,7 @@ function showPoetryPanel() {
     panel.classList.add('show-panel')
 }
 function updatePanelInfo(poetry) {
-    console.log(poetry)
     for (var key in poetry) {
-
         var element = document.getElementById('poetry-' + key);
         console.log(key)
         if (element) {
@@ -89,11 +70,41 @@ function updatePanelInfo(poetry) {
     }
 }
 
-//监听筛选
-document.getElementById('select-originName').addEventListener('change', filterPoetry);
-document.getElementById('select-emotion').addEventListener('change', filterPoetry);
-document.getElementById('select-method').addEventListener('change', filterPoetry);
-document.getElementById('select-theme').addEventListener('change', filterPoetry);
+// 监听下拉框的改变事件
+document.getElementById('select-originName').addEventListener('change', function () {
+    filterPoetry();
+    closePoetryPanel();
+});
+
+document.getElementById('select-emotion').addEventListener('change', function () {
+    filterPoetry();
+    closePoetryPanel();
+});
+
+document.getElementById('select-method').addEventListener('change', function () {
+    filterPoetry();
+    closePoetryPanel();
+});
+
+document.getElementById('select-theme').addEventListener('change', function () {
+    filterPoetry();
+    closePoetryPanel();
+});
+
+// 监听输入框的失焦事件
+var inputElement = document.getElementById('input-author');
+inputElement.addEventListener('blur', function () {
+    filterPoetry();
+    closePoetryPanel();
+});
+
+inputElement.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        filterPoetry();
+        closePoetryPanel();
+    }
+});
+
 //监听关闭右侧panel的按钮
 document.getElementById('close-btn').addEventListener('click', closePoetryPanel);
 
@@ -105,50 +116,60 @@ function getRandomPosition() {
     const y = mainRect.top + Math.random() * (mainRect.height - 100);
     return { x, y };
 }
-function createStar(p) {
-    const star = document.createElement("div");
-    star.className = "star";
-    const { x, y } = getRandomPosition();
-    star.style.left = `${x}px`;
-    star.style.top = `${y}px`;
-    const svg = document.createElement("img");
-    svg.src = "src/svg/star.svg";
-    svg.alt = "star-svg";
-    // svg.style.width = "10px"; // 设置缩小后的宽度
-    // svg.style.height = "10px"; // 设置缩小后的高度
-    svg.classList.add("star-animation");
 
-    const animationDuration = Math.random() * 5 + 2;
-    const animationDelay = Math.random();
-    svg.style.animationDuration = `${animationDuration}s`;
-    svg.style.animationDelay = `-${animationDelay}s`;
-    //添加点击效果
-    star.addEventListener("click", () => {
-        poetry = p
-    });
-
-    //添加悬浮效果
-    const infoBox = document.createElement("div");
-    infoBox.className = "info-box";
-    infoBox.innerHTML = `<p>${p.title}</p>`;
-    star.appendChild(infoBox);
-    star.appendChild(svg);
-    document.getElementById("sky-canvas").appendChild(star);
-
-    //动画最后的效果
-    void star.offsetWidth;
-    svg.style.opacity = 1;
-    // 随机设置星星的大小，最大50px，最小25px
-    const starSize = Math.floor(Math.random() * (50 - 25 + 1)) + 5;
-    svg.style.width = `${starSize}px`;
-    svg.style.height = `${starSize}px`;
-}
-function createStars(starList) {
+async function createStars(starList) {
     const existingStars = document.querySelectorAll(".star");
     existingStars.forEach((star) => star.remove());
-    starList.forEach(createStar);
+    const promises = starList.map(async (poetry) => {
+        return new Promise((resolve) => {
+            const star = document.createElement("div");
+            star.className = "star";
+            const { x, y } = getRandomPosition();
+            star.style.left = `${x}px`;
+            star.style.top = `${y}px`;
+
+            const svg = document.createElement("img");
+            svg.src = "src/svg/star.svg";
+            svg.alt = "star-svg";
+            svg.classList.add("star-animation");
+
+            const animationDuration = Math.random() * 5 + 2;
+            const animationDelay = Math.random();
+            svg.style.animationDuration = `${animationDuration}s`;
+            svg.style.animationDelay = `-${animationDelay}s`;
+
+            star.addEventListener('click', function () {
+                updatePanelInfo(poetry);
+                showPoetryPanel();
+            });
+
+            const infoBox = document.createElement("div");
+            infoBox.className = "info-box";
+            infoBox.innerHTML = `<p>${poetry.title}</p>`;
+            star.appendChild(infoBox);
+            star.appendChild(svg);
+
+            // 添加到页面
+            document.getElementById("sky-canvas").appendChild(star);
+
+            // 动画最后的效果
+            void star.offsetWidth;
+            svg.style.opacity = 1;
+
+            const starSize = Math.floor(Math.random() * (50 - 25 + 1)) + 5;
+            svg.style.width = `${starSize}px`;
+            svg.style.height = `${starSize}px`;
+
+            resolve();
+        });
+    });
+    // 等待所有的 Promise 完成
+    await Promise.all(promises);
 }
 
-// createStars(poetryArray);
+
+// 调用 filterPoetry
+filterPoetry();
+
 
 filterPoetry();
--- src/js/sankey.js
+++ src/js/sankey.js
@@ -16,15 +16,18 @@ sankeyData.nodes.forEach(node => {
         node.label.position = "top";
     } else {
         node.label.
-        position = "bottom";
+            position = "bottom";
     }
 
-     // 根据节点的y值判断是否使用本地图片
-     if (node.y === 450) {
-        node.symbol = 'image://src/image/sankey-icon/1.png',
+    // 根据节点的y值判断是否使用本地图片
+    if (node.y === 450) {
+        node.symbol = 'image://src/svg/sankey-icons/1.svg',
         node.symbolSize = [30, 30]; // 设置图片的大小
+        node.itemStyle = {
+            color: "#202020",
+        };
     }
-    
+
 });
 
 // 设置连接线颜色与源节点一样
@@ -34,6 +37,56 @@ sankeyData.links.forEach(link => {
         color: sourceNode.itemStyle ? sourceNode.itemStyle.color : 'white'
     };
 });
+
+//调整白色透明度
+sankeyData.links.forEach(link => {
+    const sourceNode = sankeyData.nodes.find(node => node.name === link.source);
+    const sourceColor = sourceNode.itemStyle ? sourceNode.itemStyle.color : 'white';
+
+    link.lineStyle = {
+        color: sourceColor,
+        opacity: sourceColor === 'white' ? 0.05 : 0.3,
+        width: 1,
+    };
+});
+
+
+
+myChart.on('mouseover', function (params) {
+    if (params.dataType === 'node') {
+        const dataName = params.data.name;
+        const linksData = sankeyData.links.filter(link => link.source === dataName || link.target === dataName);
+        linksData.forEach(link => {
+            link.lineStyle.opacity = 0.6;
+            link.lineStyle.width = 1.5
+        });
+        myChart.setOption({
+            series: [{
+                links: sankeyData.links
+            }]
+        });
+    }
+});
+
+// Add event listener for mouseout event
+myChart.on('mouseout', function () {
+    sankeyData.links.forEach(link => {
+        const sourceNode = sankeyData.nodes.find(node => node.name === link.source);
+        const sourceColor = sourceNode.itemStyle ? sourceNode.itemStyle.color : 'white';
+
+        link.lineStyle = {
+            color: sourceColor,
+            opacity: sourceColor === 'white' ? 0.05 : 0.3,
+            width: 1,
+        };
+    });
+    myChart.setOption({
+        series: [{
+            links: sankeyData.links
+        }]
+    });
+});
+
 
 var option;
 
@@ -65,7 +118,6 @@ option = {
             lineStyle: {
                 opacity: 0.3,
                 width: 1,
-                // curveness: 0,
                 color: "source"
             },
             itemStyle: {
@@ -73,10 +125,10 @@ option = {
             },
             emphasis: {
                 focus: 'adjacency',
-                lineStyle:{
+                lineStyle: {
                     opacity: 1,
                     width: 1.5,
-                }
+                },
             }
         }
     ]
@@ -85,5 +137,7 @@ option = {
 if (option && typeof option === 'object') {
     myChart.setOption(option);
 }
+
+
 
 window.addEventListener('resize', myChart.resize);
