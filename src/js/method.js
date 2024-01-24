b346a93db8ba78139ca4e7825504e1cc58aa607b
YeJingxin515 <1771357098@qq.com>
2024-01-24 11:58:20 +0800

    增加尾图 修改legend 修改饼图

--- src/js/method.js
+++ src/js/method.js
@@ -37,116 +37,6 @@ const sidePieData = {
     ]
 }
 
-// // 玫瑰数据
-// const roseData = [
-//     { value: 231, name: '山水田园' },
-//     { value: 88, name: '边塞军旅' },
-//     { value: 186, name: '咏史怀古' },
-//     { value: 140, name: '咏物言志' },
-//     { value: 203, name: '送别怀人' },
-//     { value: 69, name: '羁旅思乡' },
-//     { value: 185, name: '爱情闺怨' },
-//     { value: 466, name: '感怀诗' },
-//     { value: 255, name: '哲理诗' },
-//     { value: 234, name: '赞颂诗' },
-//     { value: 328, name: '其他' },
-// ]
-
-// //雷达图分割线数据
-// const radarIndicatorData = {
-//     '山水田园': [
-//         { name: '牛宿', max: 15 },
-//         { name: '井宿', max: 15 },
-//         { name: '斗宿', max: 15 },
-//         { name: '星宿', max: 15 },
-//         { name: '鬼宿', max: 15 },
-//     ],
-//     '爱情闺怨': [
-//         { name: '女宿', max: 20 },
-//         { name: '牛宿', max: 20 },
-//         { name: '井宿', max: 20 },
-//         { name: '斗宿', max: 20 },
-//         { name: '星宿', max: 20 },
-//     ],
-//     '边塞军旅': [
-//         { name: '鬼宿', max: 10 },
-//         { name: '牛宿', max: 10 },
-//         { name: '斗宿', max: 10 },
-//         { name: '井宿', max: 10 },
-//         { name: '轸宿', max: 10 },
-//     ],
-//     '感怀诗': [
-//         { name: '井宿', max: 25 },
-//         { name: '鬼宿', max: 25 },
-//         { name: '牛宿', max: 25 },
-//         { name: '星宿', max: 25 },
-//         { name: '女宿', max: 25 },
-//     ],
-//     '羁旅思乡': [
-//         { name: '牛宿', max: 10 },
-//         { name: '女宿', max: 10 },
-//         { name: '斗宿', max: 10 },
-//         { name: '翼宿', max: 10 },
-//         { name: '井宿', max: 10 },
-//     ],
-//     '送别怀人': [
-//         { name: '鬼宿', max: 10 },
-//         { name: '斗宿', max: 10 },
-//         { name: '女宿', max: 10 },
-//         { name: '井宿', max: 10 },
-//         { name: '牛宿', max: 10 },
-//     ],
-//     '咏史怀古': [
-//         { name: '牛宿', max: 10 },
-//         { name: '斗宿', max: 10 },
-//         { name: '星宿', max: 10 },
-//         { name: '井宿', max: 10 },
-//         { name: '女宿', max: 10 },
-//     ],
-//     '咏物言志': [
-//         { name: '牛宿', max: 10 },
-//         { name: '斗宿', max: 10 },
-//         { name: '井宿', max: 10 },
-//         { name: '鬼宿', max: 10 },
-//         { name: '女宿', max: 10 },
-//     ],
-//     '赞颂诗': [
-//         { name: '女宿', max: 15 },
-//         { name: '牛宿', max: 15 },
-//         { name: '鬼宿', max: 15 },
-//         { name: '井宿', max: 15 },
-//         { name: '斗宿', max: 15 },
-//     ],
-//     '哲理诗': [
-//         { name: '牛宿', max: 20 },
-//         { name: '星宿', max: 20 },
-//         { name: '井宿', max: 20 },
-//         { name: '室宿', max: 20 },
-//         { name: '鬼宿', max: 20 },
-//     ],
-//     '其他': [
-//         { name: '井宿', max: 15 },
-//         { name: '鬼宿', max: 15 },
-//         { name: '女宿', max: 15 },
-//         { name: '危宿', max: 15 },
-//         { name: '牛宿', max: 15 },
-//     ],
-// }
-
-// //雷达图数据
-// const radarData = {
-//     '山水田园': [[12, 10, 7, 6, 5]],
-//     '爱情闺怨': [[20, 19, 2, 1, 1]],
-//     '边塞军旅': [[9, 7, 4, 3, 3]],
-//     '感怀诗': [[21, 20, 18, 15, 12]],
-//     '羁旅思乡': [[6, 4, 4, 2, 2]],
-//     '送别怀人': [[9, 9, 8, 7, 4]],
-//     '咏史怀古': [[10, 7, 6, 6, 5]],
-//     '咏物言志': [[9, 7, 6, 3, 3]],
-//     '赞颂诗': [[13, 12, 11, 8, 5]],
-//     '哲理诗': [[16, 13, 9, 9, 7]],
-//     '其他': [[13, 12, 9, 9, 8]]
-// }
 
 
 //小的环形图色彩范围
@@ -157,9 +47,6 @@ const colorList = {
     '星为代语': ['#1F25A6', '#151A95', '#161A6D', '#090E6C', '#0A0D53']
 }
 
-//选择的情感
-var selectedMethod = ''
-var selectedEmotion = "咏史怀古"
 
 
 //绘制中间的情感饼图
@@ -223,9 +110,7 @@ function drawCenterPieChart() {
 
     myChart.on('click', function (params) {
         var name = params.name;
-        selectedMethod = name;
-        drawSidePieChart();
-        drawVector();
+        hightLightChart(name)
     });
 
     if (option && typeof option === 'object') {
@@ -236,24 +121,61 @@ function drawCenterPieChart() {
 
 }
 
+
+function hightLightChart(chartName) {
+    var chartMapping = {
+        '星为主题': { chartId: 'Side-Pie-Chart1', lineId: 'pie-pie-line1' },
+        '星为元素': { chartId: 'Side-Pie-Chart2', lineId: 'pie-pie-line2' },
+        '星为代语': { chartId: 'Side-Pie-Chart3', lineId: 'pie-pie-line3' },
+        '星为原型意象': { chartId: 'Side-Pie-Chart4', lineId: 'pie-pie-line4' }
+    };
+
+    // 恢复默认透明度
+    for (var key in chartMapping) {
+        if (chartMapping.hasOwnProperty(key)) {
+            var defaultOpacity = 0.3; // 默认透明度
+            var mapping = chartMapping[key];
+            setOpacity(mapping.chartId, mapping.lineId, defaultOpacity);
+        }
+    }
+
+    // 设置点击的图表为透明度为1
+    if (chartMapping.hasOwnProperty(chartName)) {
+        var selectedChart = chartMapping[chartName];
+        setOpacity(selectedChart.chartId, selectedChart.lineId, 1);
+    }
+}
+
+function setOpacity(chartId, lineId, opacity) {
+    const chartElement = document.getElementById(chartId);
+    const lineElement = document.getElementById(lineId);
+    if (chartElement && lineElement) {
+        chartElement.style.opacity = opacity;
+        lineElement.style.opacity = opacity;
+    }
+}
+
 //绘制边上的情感饼图
-function drawSidePieChart() {
-    var dom = document.getElementById('Side-Pie-Chart');
-    dom.style.position = 'absolute'
-    dom.style.removeProperty('top');
-    dom.style.removeProperty('right');
-    dom.style.removeProperty('left');
-    dom.style.removeProperty('bottom');
-    if (selectedMethod == '星为主题') {
+function drawSidePieChart(chartName) {
+    var dom;
+    if (chartName == '星为主题') {
+        dom = document.getElementById('Side-Pie-Chart1');
+        dom.style.position = 'absolute'
         dom.style.top = '287px';
         dom.style.left = '270px';
-    } else if (selectedMethod == '星为元素') {
+    } else if (chartName == '星为元素') {
+        dom = document.getElementById('Side-Pie-Chart2');
+        dom.style.position = 'absolute'
         dom.style.top = '166px';
         dom.style.right = '333px';
-    } else if (selectedMethod == '星为代语') {
+    } else if (chartName == '星为代语') {
+        dom = document.getElementById('Side-Pie-Chart3');
+        dom.style.position = 'absolute'
         dom.style.top = '-25px';
         dom.style.left = '518px';
-    } else if (selectedMethod == '星为原型意象') {
+    } else if (chartName == '星为原型意象') {
+        dom = document.getElementById('Side-Pie-Chart4');
+        dom.style.position = 'absolute'
         dom.style.top = '458px';
         dom.style.left = '464px';
     }
@@ -274,7 +196,7 @@ function drawSidePieChart() {
                 avoidLabelOverlap: false,
                 selectedMode: true,
                 selectedOffset: 0,
-                color: colorList[selectedMethod],
+                color: colorList[chartName],
                 label: {
                     show: false,
                 },
@@ -290,7 +212,7 @@ function drawSidePieChart() {
                 labelLine: {
                     show: false
                 },
-                data: sidePieData[selectedMethod]
+                data: sidePieData[chartName]
             }
         ]
     };
@@ -302,29 +224,34 @@ function drawSidePieChart() {
 
 }
 //绘制灰色的连接线
-async function drawVector() {
-    var piePieLine = document.getElementById('pie-pie-line');
+async function drawVector(chartName) {
+    var piePieLine;
     var src;
-    piePieLine.style.position = 'absolute'
-    piePieLine.style.removeProperty('top');
-    piePieLine.style.removeProperty('right');
-    piePieLine.style.removeProperty('left');
-    piePieLine.style.removeProperty('bottom');
-    piePieLine.style.zIndex = -1
-
-    if (selectedMethod == '星为主题') {
+    if (chartName == '星为主题') {
+        piePieLine = document.getElementById('pie-pie-line1');
+        piePieLine.style.position = 'absolute'
+        piePieLine.style.zIndex = -1
         src = './src/svg/vector4.svg'
         piePieLine.style.top = '275px';
         piePieLine.style.left = '340px';
-    } else if (selectedMethod == '星为元素') {
+    } else if (chartName == '星为元素') {
+        piePieLine = document.getElementById('pie-pie-line2');
+        piePieLine.style.position = 'absolute'
+        piePieLine.style.zIndex = -1
         src = './src/svg/vector2.svg'
         piePieLine.style.top = '174px';
         piePieLine.style.right = '400px';
-    } else if (selectedMethod == '星为代语') {
+    } else if (chartName == '星为代语') {
+        piePieLine = document.getElementById('pie-pie-line3');
+        piePieLine.style.position = 'absolute'
+        piePieLine.style.zIndex = -1
         src = './src/svg/vector1.svg'
         piePieLine.style.top = '33px';
         piePieLine.style.left = '532px';
-    } else if (selectedMethod == '星为原型意象') {
+    } else if (chartName == '星为原型意象') {
+        piePieLine = document.getElementById('pie-pie-line4');
+        piePieLine.style.position = 'absolute'
+        piePieLine.style.zIndex = -1
         src = './src/svg/vector3.svg'
         piePieLine.style.top = '375px';
         piePieLine.style.left = '500px';
@@ -336,181 +263,17 @@ async function drawVector() {
     } catch (error) {
         console.error('加载SVG时发生错误:', error);
     }
-
-    var methodChartWarp = document.getElementById('method-chart-warp');
-    methodChartWarp.appendChild(piePieLine);
 }
 
-// function drawRoseChart() {
-//     var dom = document.getElementById('Rose-Chart');
-//     var myChart = echarts.init(dom, null, {
-//         renderer: 'svg',
-//         useDirtyRect: false
-//     });
-//     var option;
-//     option = {
-//         title: {
-//             text: "图 星象与诗的题材",
-//             textStyle: {
-//                 color: "#fff",
-//                 fontSize: 16,
-//                 fontFamily: 'SongTi-regular',
-//                 fontWeight: 'normal',
-//                 lineHeight: 30,
-//             },
-//             left: 'center',
-//             bottom: 0
-//         },
-//         tooltip: {
-//             trigger: 'item',
-//         },
-//         series: [
-//             {
-//                 name: '诗的题材',
-//                 type: 'pie',
-//                 radius: ['40%', '90%'],
-//                 roseType: 'area',
-//                 avoidLabelOverlap: false,
-//                 selectedMode: true,
-//                 selectedOffset: 0,
-//                 label: {
-//                     position: 'center',
-//                     show: false
-//                 },
-//                 color: ['#BBD0E9', '#92C3E7', '#64ACE1', '#1678CC', '#0054C2', '#003384', '#00264E', '#0054C2', '#60B4E2', '#9ECFE3', '#CEE7F2'],
-//                 itemStyle: {
-//                     borderColor: '#fff', // 默认描边颜色
-//                     borderWidth: 0.3, // 默认描边宽度
-//                     opacity: 0.5
-
-//                 },
-//                 select: {
-//                     itemStyle: {
-//                         borderColor: '#fff',
-//                         borderWidth: 2,
-//                         opacity: 1
-//                     },
-//                     label: {
-//                         show: true,
-//                         fontSize: 16,
-//                         fontFamily: 'SongTi-regular',
-//                         color: "#fff",
-//                         backgroundColor: {
-//                             image: 'src/image/blue-star3.png',
-//                         },
-//                         width: 106,
-//                         height: 106,
-//                     },
-//                 },
-//                 emphasis: {
-//                     label: {
-//                         show: true,
-//                         fontSize: 16,
-//                         fontFamily: 'SongTi-regular',
-//                         color: "#fff",
-//                         backgroundColor: {
-//                             image: 'src/image/blue-star3.png',
-//                         },
-//                         width: 106,
-//                         height: 106,
-//                     },
-//                     itemStyle: {
-//                         borderColor: '#fff',
-//                         borderWidth: 2
-//                     }
-//                 },
-//                 labelLine: {
-//                     show: false
-//                 },
-//                 data: roseData
-//             }
-//         ]
-//     };
-
-
-//     myChart.on('click', function (params) {
-//         var name = params.name;
-//         selectedEmotion = name;
-//         myChart.dispatchAction({ type: "downplay", seriesIndex: 0, dataIndex: 2 });
-//         drawRadarChart()
-//     });
-
-
-//     if (option && typeof option === 'object') {
-//         myChart.setOption(option);
-//     }
-//     myChart.dispatchAction({ type: "highlight", seriesIndex: 0, dataIndex: 2 }); //设置默认选中高亮部分
-//     window.addEventListener('resize', myChart.resize);
-// }
-
-// //绘制雷达图
-// function drawRadarChart() {
-//     var dom = document.getElementById('Radar-Chart');
-//     var myChart = echarts.init(dom, null, {
-//         renderer: 'svg',
-//         useDirtyRect: false
-//     });
-
-//     var option;
-//     option = {
-//         tooltip: {
-//             trigger: 'item',
-//         },
-//         radar: {
-//             indicator: radarIndicatorData[selectedEmotion],
-//             shape: 'circle',
-//             axisName: {
-//                 color: '#fff',
-//                 fontFamily: 'SongTi-regular',
-//                 fontSize: 14
-//             },
-//             splitLine: {
-//                 lineStyle: {
-//                     color: "#fff",
-//                     width: 0.3,
-//                 }
-//             },
-//             splitArea: {
-//                 show: true,
-//                 areaStyle: {
-//                     color: 'rgb(255,255,255,0.1)'
-//                 }
-//             },
-//             axisLine: {
-//                 lineStyle: {
-//                     color: '#fff',
-//                     width: 0.3
-//                 }
-//             }
-//         },
-//         series: [
-//             {
-//                 name: selectedEmotion,
-//                 type: 'radar',
-//                 lineStyle: {
-//                     width: 1,
-//                     opacity: 1
-//                 },
-//                 data: radarData[selectedEmotion],
-//                 symbol: 'none',
-//                 itemStyle: {
-//                     color: '#2347C4',
-//                 },
-//                 areaStyle: {
-//                     opacity: 0.4,
-//                 }
-//             }
-
-//         ]
-//     };
-
-//     if (option && typeof option === 'object') {
-//         myChart.setOption(option);
-//     }
-
-//     window.addEventListener('resize', myChart.resize);
-// }
 
 drawCenterPieChart();
-// drawRoseChart();
-// drawRadarChart();
+
+
+drawSidePieChart('星为主题');
+drawSidePieChart('星为元素');
+drawSidePieChart('星为代语');
+drawSidePieChart('星为原型意象');
+drawVector('星为主题');
+drawVector('星为元素');
+drawVector('星为代语');
+drawVector('星为原型意象');
--- src/js/sankey.js
+++ src/js/sankey.js
@@ -67,6 +67,7 @@ myChart.on('click', function (params) {
                     opacity: sourceColor === 'white' ? 0.05 : 0.3,
                     width: 1,
                 };
+                link.label = { show: false};
             });
             myChart.setOption({
                 series: [{
@@ -81,8 +82,18 @@ myChart.on('click', function (params) {
                 const sourceColor = sourceNode.itemStyle ? sourceNode.itemStyle.color : 'white';
                 link.lineStyle = {
                     opacity: link.source === tmpNode || link.target === tmpNode ? 0.6 : 0.05,
-                    width: link.source === tmpNode || link.target === tmpNode ? 1.5 : 0.5,
+                    width: link.source === tmpNode || link.target === tmpNode ? 1 : 0.5,
                     color: sourceColor
+                };
+                link.label = { // 显示高亮连线的 label
+                    show: link.source === tmpNode || link.target === tmpNode,
+                    position: "start" ,
+                    formatter: function (params) {
+                        return `${params.data.value}`;
+                    },
+                    fontFamily: "SongTi-regular",
+                    color: "#fff",
+                    fontSize: 12
                 };
             });
             myChart.setOption({
@@ -92,7 +103,6 @@ myChart.on('click', function (params) {
                 }]
             });
         }
-
     }
 });
 
@@ -106,7 +116,7 @@ myChart.on('mouseover', function (params) {
                 const sourceColor = sourceNode.itemStyle ? sourceNode.itemStyle.color : 'white';
                 link.lineStyle = {
                     opacity: link.source === dataName || link.target === dataName ? 0.6 : 0.05,
-                    width: link.source === dataName || link.target === dataName ? 1.5 : 1,
+                    width: link.source === dataName || link.target === dataName ? 1 : 0.5,
                     color: sourceColor
                 };
             });
